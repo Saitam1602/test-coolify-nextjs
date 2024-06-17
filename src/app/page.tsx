0af5769291f8bffa4459b600900c97ec4a603845
Saitam1602 <matias.maiorano@icloud.com>
2024-06-17 22:23:33 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="flex m-auto text-4xl font-bold">Ciao Titti✨</h1>
+      <h1 className="flex m-auto text-4xl font-bold">Ciao Titi✨</h1>
     </main>
   );
 }
